e799ef99e7a1b4216f586ff9c014a7e385a1b90c
sampat-talentmicro <sampat@talentmicro.com>
2024-05-02 17:52:19 +0530

    basic method setup for all created

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -293,9 +293,7 @@ export class AppComponent implements OnInit {
   }
 
   onChangeLoad(event: any) {
-    console.log('====================================');
-    console.log('file input');
-    console.log('====================================');
+
     readDocument(event);
   }
 
@@ -367,7 +365,268 @@ export class AppComponent implements OnInit {
     <img  id="resumeViewer" frameborder="0" style="width: 80px; height: 40px; object-fit: contain;" src="https://storage.googleapis.com/ezeone/icanrefer/6068f27a-e72f-4e96-90ba-ada65ce350b1.png?X-Goog-Algorithm=GOOG4-RSA-SHA256&amp;X-Goog-Credential=478594593338-74uha9dmfejgd8nt1d7hm01d72tbj6or%40developer.gserviceaccount.com%2F20240502%2Fauto%2Fstorage%2Fgoog4_request&amp;X-Goog-Date=20240502T115215Z&amp;X-Goog-Expires=3540&amp;X-Goog-SignedHeaders=host&amp;X-Goog-Signature=659485032904c9f5cfe1c30e8801a4d90c74c8fe0f36153ef000bb644b875da2409610ad90a3ea36641481c344d307e12768daf779ca10d17de473ff8d9b2f613e98e44f162643229a918769df5e53d9d259a57c5cf4d28b44174b8f068dd710ca29a7bda2279c9afdd34b2c2880984ae3bd54aff0fb672c5fb2df0989c71f01e8e8c4026c2abc4ef510dc10e54d9c0fb0503c7da9a049dad395c01e10ad1d99e717acd15fe363c31d0a0197c3a0847c178cc1b9660b68f79b98187d44597f8f5d038a1697246588e69eb33114b1c700029c322498e0b86ded527895279709a24566086d8862032076fbf15f0ef4904fe89c2e21900dbed841dcb048f7eeb24c" title="demo-logo2.png" class="ng-star-inserted">
     </div>
         <header>
-            <h3>Uzair Jukaku</h3>
+            <h3>Ashish Gupta</h3>
+            <p>Software Developer</p>
+            <p>Email: uzairj46@gmail.com | Phone: +971527875808 | Location: Dubai,Uae</p>
+        </header>
+        <hr>
+        <div class="section">
+            <h3>Summary</h3>
+            <p>Full Stack Developer with 3+ years experience in web & mobile app development. Skilled in front-end
+                (HTML,
+                CSS, JS, Angular, React, React Native) & back-end (Node,Express.js). Passionate about creating
+                user-friendly,
+                high-quality applications. Always up-to-date on latest trends & best practices.</p>
+        </div>
+        <div class="section">
+            <h3>Skills</h3>
+            <p>Java,Angular,Script writing</p>
+        </div>
+
+        <div class="section">
+            <h3>Skills</h3>
+            <table width="100%" >
+            <thead>
+            <tr>
+                  <th>
+                      Skill
+                  </th>
+                  <th>
+                      Level
+                  </th>
+                  <th>
+                      Experience
+                  </th>
+                  <th>
+                      Status
+                  </th>
+            </tr>
+            </thead>
+
+            <tbody>
+                  <tr>
+                  <td>Java</td>
+                  <td> <img  id="resumeViewer" frameborder="0" style="width: 140px; height: 20px; object-fit: contain;" src="https://storage.googleapis.com/ezeone/icanrefer/5eeca1c5-af34-4c2b-98e8-2473432e2cfa.png" title="demo-logo2.png" class="ng-star-inserted"></td>
+                  <td>3Year</td>
+                  <td>Active</td>
+                  </tr>
+            </tbody>
+            </table>
+        </div>
+        <div class="section">
+            <h3>Experience</h3>
+            <h5>Frontend Developer</h5>
+            <h6>Banke International Properties | Sep 2022 - Present (1 year 8 months)</h6>
+            <ul>
+                <li>
+                    Front End Developer at Banke International in Dubai
+                    *BankeNow web & mobile app for property listings, leasing, buying & selling
+                    *ERP web app with HRMS, recruitment & CRM functionalities
+                    *Improved design for existing systems with better UI/UX
+                    *Collaborated with team, translated designs to code & integrated front-end with back-end
+                    *Delivered high-quality results meeting client requirements & exceeding user expectations
+                </li>
+
+            </ul>
+        </div>
+
+    </div>
+</body>
+
+</html>
+`
+      )
+    );
+  }
+
+  loadDocument1() {
+    TXTextControl.loadDocument(
+      TXTextControl.StreamType.HTMLFormat,
+      btoa(
+        `
+        <!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Software Developer CV</title>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Montagu+Slab:wght@100;200;300;400;500;600;700;800;900&family=Montserrat:wght@100;200;300;400;500;600;700;800;900&display=swap"
+        rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            line-height: 1.6;
+            margin: 0;
+            padding: 0;
+        }
+
+        .container {
+            padding: 5px;
+            border: 1px solid #ccc;
+            border-radius: 8px;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+        }
+
+        h3,
+        h2 {
+            margin-top: 0px;
+            margin-bottom: .5rem;
+        }
+
+        h5,
+        h6 {
+            margin: 0px .5rem;
+        }
+
+        p,
+        ul,
+        li {
+            margin: 0px;
+            font-size: 13px;
+        }
+    </style>
+</head>
+
+<body>
+    <div class="container">
+    <div style='text-align:right'>
+    <img  id="resumeViewer" frameborder="0" style="width: 80px; height: 40px; object-fit: contain;" src="https://storage.googleapis.com/ezeone/icanrefer/6068f27a-e72f-4e96-90ba-ada65ce350b1.png?X-Goog-Algorithm=GOOG4-RSA-SHA256&amp;X-Goog-Credential=478594593338-74uha9dmfejgd8nt1d7hm01d72tbj6or%40developer.gserviceaccount.com%2F20240502%2Fauto%2Fstorage%2Fgoog4_request&amp;X-Goog-Date=20240502T115215Z&amp;X-Goog-Expires=3540&amp;X-Goog-SignedHeaders=host&amp;X-Goog-Signature=659485032904c9f5cfe1c30e8801a4d90c74c8fe0f36153ef000bb644b875da2409610ad90a3ea36641481c344d307e12768daf779ca10d17de473ff8d9b2f613e98e44f162643229a918769df5e53d9d259a57c5cf4d28b44174b8f068dd710ca29a7bda2279c9afdd34b2c2880984ae3bd54aff0fb672c5fb2df0989c71f01e8e8c4026c2abc4ef510dc10e54d9c0fb0503c7da9a049dad395c01e10ad1d99e717acd15fe363c31d0a0197c3a0847c178cc1b9660b68f79b98187d44597f8f5d038a1697246588e69eb33114b1c700029c322498e0b86ded527895279709a24566086d8862032076fbf15f0ef4904fe89c2e21900dbed841dcb048f7eeb24c" title="demo-logo2.png" class="ng-star-inserted">
+    </div>
+        <header>
+            <h3>Ashish Gupta</h3>
+            <p>Software Developer</p>
+            <p>Email: uzairj46@gmail.com | Phone: +971527875808 | Location: Dubai,Uae</p>
+        </header>
+        <hr>
+        <div class="section">
+            <h3>Summary</h3>
+            <p>Full Stack Developer with 3+ years experience in web & mobile app development. Skilled in front-end
+                (HTML,
+                CSS, JS, Angular, React, React Native) & back-end (Node,Express.js). Passionate about creating
+                user-friendly,
+                high-quality applications. Always up-to-date on latest trends & best practices.</p>
+        </div>
+        <div class="section">
+            <h3>Skills</h3>
+            <p>Java,Angular,Script writing</p>
+        </div>
+
+        <div class="section">
+            <h3>Skills</h3>
+            <table width="100%" >
+            <thead>
+            <tr>
+                  <th>
+                      Skill
+                  </th>
+                  <th>
+                      Level
+                  </th>
+                  <th>
+                      Experience
+                  </th>
+                  <th>
+                      Status
+                  </th>
+            </tr>
+            </thead>
+
+            <tbody>
+                  <tr>
+                  <td>Java</td>
+                  <td> <img  id="resumeViewer" frameborder="0" style="width: 140px; height: 20px; object-fit: contain;" src="https://storage.googleapis.com/ezeone/icanrefer/5eeca1c5-af34-4c2b-98e8-2473432e2cfa.png" title="demo-logo2.png" class="ng-star-inserted"></td>
+                  <td>3Year</td>
+                  <td>Active</td>
+                  </tr>
+            </tbody>
+            </table>
+        </div>
+        <div class="section">
+            <h3>Experience</h3>
+            <h5>Frontend Developer</h5>
+            <h6>Banke International Properties | Sep 2022 - Present (1 year 8 months)</h6>
+            <ul>
+                <li>
+                    Front End Developer at Banke International in Dubai
+                    *BankeNow web & mobile app for property listings, leasing, buying & selling
+                    *ERP web app with HRMS, recruitment & CRM functionalities
+                    *Improved design for existing systems with better UI/UX
+                    *Collaborated with team, translated designs to code & integrated front-end with back-end
+                    *Delivered high-quality results meeting client requirements & exceeding user expectations
+                </li>
+
+            </ul>
+        </div>
+
+    </div>
+</body>
+
+</html>
+`
+      )
+    );
+  }
+  loadDocument3() {
+    TXTextControl.loadDocument(
+      TXTextControl.StreamType.HTMLFormat,
+      btoa(
+        `
+        <!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Software Developer CV</title>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Montagu+Slab:wght@100;200;300;400;500;600;700;800;900&family=Montserrat:wght@100;200;300;400;500;600;700;800;900&display=swap"
+        rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            line-height: 1.6;
+            margin: 0;
+            padding: 0;
+        }
+
+        .container {
+            padding: 5px;
+            border: 1px solid #ccc;
+            border-radius: 8px;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+        }
+
+        h3,
+        h2 {
+            margin-top: 0px;
+            margin-bottom: .5rem;
+        }
+
+        h5,
+        h6 {
+            margin: 0px .5rem;
+        }
+
+        p,
+        ul,
+        li {
+            margin: 0px;
+            font-size: 13px;
+        }
+    </style>
+</head>
+
+<body>
+    <div class="container">
+    <div style='text-align:right'>
+    <img  id="resumeViewer" frameborder="0" style="width: 80px; height: 40px; object-fit: contain;" src="https://storage.googleapis.com/ezeone/icanrefer/6068f27a-e72f-4e96-90ba-ada65ce350b1.png?X-Goog-Algorithm=GOOG4-RSA-SHA256&amp;X-Goog-Credential=478594593338-74uha9dmfejgd8nt1d7hm01d72tbj6or%40developer.gserviceaccount.com%2F20240502%2Fauto%2Fstorage%2Fgoog4_request&amp;X-Goog-Date=20240502T115215Z&amp;X-Goog-Expires=3540&amp;X-Goog-SignedHeaders=host&amp;X-Goog-Signature=659485032904c9f5cfe1c30e8801a4d90c74c8fe0f36153ef000bb644b875da2409610ad90a3ea36641481c344d307e12768daf779ca10d17de473ff8d9b2f613e98e44f162643229a918769df5e53d9d259a57c5cf4d28b44174b8f068dd710ca29a7bda2279c9afdd34b2c2880984ae3bd54aff0fb672c5fb2df0989c71f01e8e8c4026c2abc4ef510dc10e54d9c0fb0503c7da9a049dad395c01e10ad1d99e717acd15fe363c31d0a0197c3a0847c178cc1b9660b68f79b98187d44597f8f5d038a1697246588e69eb33114b1c700029c322498e0b86ded527895279709a24566086d8862032076fbf15f0ef4904fe89c2e21900dbed841dcb048f7eeb24c" title="demo-logo2.png" class="ng-star-inserted">
+    </div>
+        <header>
+            <h3>Ashish Gupta</h3>
             <p>Software Developer</p>
             <p>Email: uzairj46@gmail.com | Phone: +971527875808 | Location: Dubai,Uae</p>
         </header>
